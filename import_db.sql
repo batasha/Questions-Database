5ab32a2466518a3e9ff5de192193e95bf96b0cf7
Ke and Natasha <Ke+Natasha@appacademy.io>
2013-05-02 10:58:36 -0400

    Add records to database and start on Ruby file

--- import_db.sql
+++ import_db.sql
@@ -34,9 +34,11 @@ CREATE TABLE replies (
   id INTEGER PRIMARY KEY,
   body TEXT NOT NULL,
   subject_id INTEGER NOT NULL,
+  author_id INTEGER NOT NULL,
   parent_id INTEGER,
 
   FOREIGN KEY(subject_id) REFERENCES questions(id),
+  FOREIGN KEY(author_id) REFERENCES users(id),
   FOREIGN KEY(parent_id) REFERENCES replies(id)
 );
 
@@ -51,4 +53,34 @@ CREATE TABLE question_likes (
   FOREIGN KEY(question_id) REFERENCES questions(id)
 );
 
-INSERT INTO
+INSERT INTO users ('fname', 'lname')
+VALUES ('Ke', 'Li'),
+       ('Natasha', 'Badillo');
+
+INSERT INTO questions ('title', 'body', 'author_id')
+VALUES (
+  'Monty Python Question',
+  'What is the airspeed velocity of an unladen swallow?',
+  1
+);
+
+INSERT INTO replies ('body', 'subject_id', 'author_id')
+VALUES (
+  'What do you mean?  An African or European swallow?',
+  1,
+  2
+);
+
+INSERT INTO replies('body', 'subject_id', 'author_id', 'parent_id')
+VALUES (
+  "I don't know that",
+  1,
+  1,
+  1
+);
+
+INSERT INTO question_followers('user_id', 'question_id')
+VALUES (1, 1), (2, 1);
+
+INSERT INTO question_likes('user_id', 'question_id')
+VALUES (1, 1), (2, 1);
